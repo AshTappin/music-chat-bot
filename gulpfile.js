acd74d77112bceea3645fbedadcc1a65a0e198f4
AshTappin <ashtappin1592@hotmail.com>
2016-10-23 19:32:05 +0100

    Add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-// Include gulp
 var gulp = require('gulp');
 
-// Include Our Plugins
+var watch = require('gulp-watch');
+var jasmine = require('gulp-jasmine');
 var jshint = require('gulp-jshint');
 
-// Lint Task
 gulp.task('lint', function() {
     return gulp.src('*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('tests.run', function() {
+	return gulp.src('./responseHandlers/*.spec.js')
+		.pipe(jasmine({verbose: true}));
+
+});
+
+gulp.task('tests.watch', function() {
+	gulp.run('tests.run')
+	gulp.watch('./responseHandlers/*.js', ['tests.run']);
 });
