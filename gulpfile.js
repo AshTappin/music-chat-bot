7a533db486206816316798f513c258622f199475
AshTappin <ashtappin1592@hotmail.com>
2016-10-23 20:54:01 +0100

    Add subscribeApp gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var jasmine = require('gulp-jasmine');
 var jshint = require('gulp-jshint');
+const apiKey = require('./config.js').messenger.apikey;
+const exec = require('child_process').exec;
 
 gulp.task('lint', function() {
     return gulp.src('*.js')
@@ -20,3 +22,11 @@ gulp.task('tests.watch', function() {
 	gulp.run('tests.run')
 	gulp.watch('./responseHandlers/*.js', ['tests.run']);
 });
+
+gulp.task('suscribeApp', function (cb) {
+  exec(`curl -ik -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=${apiKey}"`, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
